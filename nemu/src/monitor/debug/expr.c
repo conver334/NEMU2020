a2909ad78a786bf1997ac2c3887f732a3c0ccf3a
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-07 06:08:32 +0000

    >  compile NEMU 3018216334 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:08:32 up 21:02,  1 user,  load average: 0.66, 0.73, 0.78 16bef208bb7e42efa7c13d949a9aa588b3191c9f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -83,9 +83,10 @@ static bool make_token(char *e) {
 				 * of tokens, some extra actions should be performed.
 				 */
 
-				switch(rules[i].token_type) {
-					default: panic("please implement me");
-				}
+				// switch(rules[i].token_type) {
+
+				// 	//default: panic("please implement me");
+				// }
 
 				break;
 			}
