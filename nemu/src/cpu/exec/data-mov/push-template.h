ca7fd1be7ee993b7e3d8c678626b51c5361b2828
tracer-ics2016 <tracer@njuics.org>
2020-09-22 13:13:25 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:13:25 up 7 days, 10:55,  1 user,  load average: 1.52, 1.21, 1.03 967c1f90c0c4c60763f68bf287b44de2880d50a

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -6,10 +6,11 @@ static void do_execute(){
     //1. R[ESP]<-- R[ESP]-4
     //2. send a word or double word from the register to the stack indicated by ESP
     reg_l(R_ESP)-=4;
-    swaddr_write(reg_l (R_ESP),4,op_src->val);
+    swaddr_write(reg_l(R_ESP),4,op_src->val);
     print_template1();
 }
 
-make_instr_helper(rm)
 make_instr_helper(r)
+make_instr_helper(rm)
+
 #include "cpu/exec/template-end.h"
