7a3dde28150444858766e371e7a3d1163fd7b0fb
tracer-ics2016 <tracer@njuics.org>
2020-09-28 16:20:15 +0800

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:20:15 up 18:53,  1 user,  load average: 0.72, 0.62, 0.45 e6a256cd7f14cd014d61e5c58ae7b18deb066eb

--- nemu/src/cpu/exec/logic/shl-template.h
+++ nemu/src/cpu/exec/logic/shl-template.h
@@ -6,13 +6,24 @@ static void do_execute () {
 	DATA_TYPE src = op_src->val;
 	DATA_TYPE dest = op_dest->val;
 
+	
+
+	/* TODO: Update EFLAGS. */
+	int len = (DATA_BYTE << 3) - 1;
+	cpu.CF=dest>>len;
 	uint8_t count = src & 0x1f;
 	dest <<= count;
 	OPERAND_W(op_dest, dest);
-
-	/* TODO: Update EFLAGS. */
-	panic("shl is not defined");
-
+	
+	cpu.OF=((dest>>len)!=cpu.CF);
+	cpu.SF=dest >> len;
+	cpu.ZF=!dest;
+	
+    int i, flag=1;
+    for(i=0;i<8;i++){
+        if(dest&(1<<i))flag=flag^1;
+    }
+    cpu.PF=flag;
 	print_asm_template2();
 }
 
--- nemu/src/cpu/exec/logic/shr-template.h
+++ nemu/src/cpu/exec/logic/shr-template.h
@@ -7,12 +7,24 @@ static void do_execute () {
 	DATA_TYPE dest = op_dest->val;
 
 	uint8_t count = src & 0x1f;
-	dest >>= count;
-	OPERAND_W(op_dest, dest);
+	
 
 	/* TODO: Update EFLAGS. */
-	panic("shr is not defined");
-
+	
+	cpu.CF= dest&1;
+	dest >>= count;
+	OPERAND_W(op_dest, dest);
+	int len = (DATA_BYTE << 3) - 1;
+	cpu.OF=src>>len;
+	cpu.SF=dest >> len;
+	cpu.ZF=!dest;
+	
+    int i, flag=1;
+    for(i=0;i<8;i++){
+        if(dest&(1<<i))flag=flag^1;
+    }
+    cpu.PF=flag;
+	print_asm_template2();
 	print_asm_template2();
 }
 
