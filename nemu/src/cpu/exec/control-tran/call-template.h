246bbb76fbfeaadccdc9466f507f5fd3bd111f60
tracer-ics2016 <tracer@njuics.org>
2020-09-29 11:00:41 +0800

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:00:41 up 1 day, 13:34,  1 user,  load average: 2.02, 1.60, 1.05 f767620fa4ea76cf7fb4bc8e6a8ce3bded6df13c

--- nemu/src/cpu/exec/control-tran/call-template.h
+++ nemu/src/cpu/exec/control-tran/call-template.h
@@ -4,21 +4,12 @@
 
 make_helper (concat(call_i_, SUFFIX))
 {
-	//concat string splicing  
-	//eip where the program is now
 	int len = concat(decode_i_, SUFFIX) (eip + 1);
-	//esp stack top
 	reg_l (R_ESP) -= DATA_BYTE;
-	//push the return address onto the stack [1.the stack needle moves down,then write ].
 	swaddr_write (reg_l (R_ESP) , 4 , cpu.eip + len);
-	printf("%x\n",cpu.eip + len);
-	//after decode
-	//op_src immediate information in the instruction
-	//op_dest register information
-	DATA_TYPE_S jumplen = op_src->val;
-	//jump to the target address
-	cpu.eip +=jumplen;
-	print_asm("call %x",cpu.eip + 1 + len);
+	DATA_TYPE_S displacement = op_src->val;
+	print_asm("call %x",cpu.eip + 1 + len + displacement);
+	cpu.eip +=displacement;
 	return len + 1;
 }
 make_helper (concat(call_rm_, SUFFIX))
@@ -26,12 +17,33 @@ make_helper (concat(call_rm_, SUFFIX))
 	int len = concat(decode_rm_, SUFFIX) (eip + 1);
 	reg_l (R_ESP) -= DATA_BYTE;
 	swaddr_write (reg_l (R_ESP) , 4 , cpu.eip + len);
-	DATA_TYPE_S jumplen = op_src->val;
-	cpu.eip = jumplen - len - 1;
-	print_asm("call %x",jumplen);
+	DATA_TYPE_S displacement = op_src->val;
+	print_asm("call %x",displacement);
+	cpu.eip = displacement - len - 1;
 	return len + 1;
 }
 
 
 
 #include "cpu/exec/template-end.h"
+
+
+// make_helper (concat(call_i_, SUFFIX))
+// {
+// 	//concat string splicing  
+// 	//eip where the program is now
+// 	int len = concat(decode_i_, SUFFIX) (eip + 1);
+// 	//esp stack top
+// 	reg_l (R_ESP) -= DATA_BYTE;
+// 	//push the return address onto the stack [1.the stack needle moves down,then write ].
+// 	swaddr_write (reg_l (R_ESP) , 4 , cpu.eip + len);
+// 	printf("%x\n",cpu.eip + len);
+// 	//after decode
+// 	//op_src immediate information in the instruction
+// 	//op_dest register information
+// 	DATA_TYPE_S jumplen = op_src->val;
+// 	//jump to the target address
+// 	cpu.eip +=jumplen;
+// 	print_asm("call %x",cpu.eip + 1 + len);
+// 	return len + 1;
+// }
