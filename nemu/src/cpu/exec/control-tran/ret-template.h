6e9476a7a65f974037cfe476c84b43e16af9da1a
tracer-ics2016 <tracer@njuics.org>
2020-09-29 11:01:43 +0800

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:01:43 up 1 day, 13:35,  1 user,  load average: 1.97, 1.64, 1.10 abac9e20374522f3854ff6ab1b977d50c84f77d9

--- nemu/src/cpu/exec/control-tran/ret-template.h
+++ nemu/src/cpu/exec/control-tran/ret-template.h
@@ -1,24 +1,52 @@
+// #include "cpu/exec/template-start.h"
+
+// #define instr ret
+
+// make_helper(concat(ret_n_, SUFFIX)) {
+// 	cpu.eip = MEM_R(reg_l(R_ESP)) ;
+// 	reg_l(R_ESP) += DATA_BYTE;
+// 	print_asm("ret");
+// 	return 1;
+// }
+
+// make_helper(concat(ret_i_, SUFFIX)) {
+// 	uint32_t num = instr_fetch(eip + 1, 2);
+// 	cpu.eip = MEM_R (REG (R_ESP));
+// 	if (DATA_BYTE == 2)cpu.eip &= 0xffff;
+// 	reg_l(R_ESP) += DATA_BYTE;
+// 	int i;
+// 	for (i = 0;i < num; i+=DATA_BYTE)
+// 	MEM_W (REG (R_ESP) + i,0);
+// 	reg_l(R_ESP) += num;
+// 	print_asm("ret 0x%x", num);
+// 	return 1;
+// }
+// #include "cpu/exec/template-end.h"
 #include "cpu/exec/template-start.h"
 
 #define instr ret
 
-make_helper(concat(ret_n_, SUFFIX)) {
-	cpu.eip = MEM_R(reg_l(R_ESP)) ;
-	reg_l(R_ESP) += DATA_BYTE;
+make_helper(concat(ret_n_, SUFFIX))
+{
+	cpu.eip = MEM_R (reg_l (R_ESP));
+	//if (DATA_BYTE == 2)cpu.eip &= 0xffff;
+	reg_l (R_ESP) += DATA_BYTE;
 	print_asm("ret");
 	return 1;
 }
 
-make_helper(concat(ret_i_, SUFFIX)) {
-	uint32_t num = instr_fetch(eip + 1, 2);
+make_helper(concat(ret_i_, SUFFIX))
+{
+	int val = instr_fetch(eip + 1, 2);
+	int i;
 	cpu.eip = MEM_R (REG (R_ESP));
 	if (DATA_BYTE == 2)cpu.eip &= 0xffff;
-	reg_l(R_ESP) += DATA_BYTE;
-	int i;
-	for (i = 0;i < num; i+=DATA_BYTE)
+	REG (R_ESP) += DATA_BYTE;
+	for (i = 0;i < val; i+=DATA_BYTE)
 	MEM_W (REG (R_ESP) + i,0);
-	reg_l(R_ESP) += num;
-	print_asm("ret 0x%x", num);
+	REG (R_ESP) += val;
+	print_asm("ret $0x%x",val);
 	return 1;
 }
+
 #include "cpu/exec/template-end.h"
