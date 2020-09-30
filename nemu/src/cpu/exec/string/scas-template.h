408ea9abf60135cc0e3b12d1c5b207a2bd9748a4
tracer-ics2016 <tracer@njuics.org>
2020-09-30 21:55:56 +0800

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:55:56 up 3 days, 29 min,  1 user,  load average: 0.54, 0.93, 1.21 acb3086a6d346aa4626869d6163a0a776adda91

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -4,20 +4,20 @@
 
 make_helper(concat(scas_, SUFFIX)) {
 	swaddr_t s1 = REG(R_EAX), s2 = swaddr_read(reg_l(R_EDI), DATA_BYTE);
-	uint32_t res = s1 - s2;
+	// uint32_t res = s1 - s2;
 	if (cpu.DF == 0) reg_l(R_EDI) += DATA_BYTE;
 	else reg_l(R_EDI) -= DATA_BYTE;
 	// concat(updateCPU_, SUFFIX) (res);
 	int len = (DATA_BYTE << 3) - 1;
 	cpu.CF = s1 < s2;
-	cpu.ZF = !res;
+	// cpu.ZF = !res;
     cpu.OF = ((s1 >> len) != (s2 >> len) && (s2 >> len) == cpu.SF);
-	int i, flag=1;
-	cpu.SF = res>>len;
-    for(i=0;i<8;i++){
-        if(res&(1<<i))flag=flag^1;
-    }
-    cpu.PF=flag;
+	// int i, flag=1;
+	// cpu.SF = res>>len;
+    // for(i=0;i<8;i++){
+    //     if(res&(1<<i))flag=flag^1;
+    // }
+    // cpu.PF=flag;
 	print_asm("scas%s", str(SUFFIX));
 
 	return 1;
