771ea9602485a1cf2a9134ba1a22a05fe8147e69
tracer-ics2016 <tracer@njuics.org>
2020-09-30 23:00:06 +0800

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:00:06 up 3 days,  1:33,  1 user,  load average: 1.83, 1.46, 1.19 221ba03549c9160bb76bd0b93fc9b925e5c9e02a

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -4,24 +4,19 @@
 
 static void do_execute() {
 	DATA_TYPE result = op_dest->val + op_src->val;
-	OPERAND_W(op_dest, result);
 	int len = (DATA_BYTE << 3) - 1;
+	int s1,s2;
 	cpu.CF=(result < op_dest->val);
 	cpu.SF=result >> len;
-
-    int op1,op2;
-	op1=op_dest->val>>len;
-	op2=op_src->val>>len;
-    cpu.OF=(op1 == op2 && op1 != cpu.SF) ;
-
+	s1=op_dest->val>>len;
+	s2=op_src->val>>len;
+    	cpu.OF=(s1 == s2 && s1 != cpu.SF) ;
 	cpu.ZF=!result;
-	
-    int i, flag=1;
-    for(i=0;i<8;i++){
-        if(result&(1<<i))flag=flag^1;
-    }
-    cpu.PF=flag;
-	
+	OPERAND_W(op_dest, result);
+	result ^= result >>4;
+	result ^= result >>2;
+	result ^= result >>1;
+	cpu.PF=!(result & 1);
 	print_template2();
 }
 #if DATA_BYTE == 2 || DATA_BYTE == 4
