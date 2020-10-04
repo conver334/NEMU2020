9687df5a2976f91d3a2b7bf1a80455c6a22e1177
tracer-ics2016 <tracer@njuics.org>
2020-10-04 21:25:17 +0800

    >  run 151220000 snow Linux snowNEMU 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:25:17 up  6:11,  1 user,  load average: 0.59, 0.51, 0.36 2cad31be66185da1409395176f9eaa7772c0ec3d

---
+++ testcase/src/mytest.c
@@ -0,0 +1,45 @@
+#include "trap.h"
+int n,L,f[1010];
+struct node{
+	int s,t;
+}a[1010];
+int max(int x,int y){
+	return x>y?x:y;
+}
+int main(){
+	n=16,L=10;
+	int i,j;
+	for( i=1;i<=n;++i)a[i].s=1, a[i].t=1;
+	int ans = 0,w;
+
+	for(i = 1; i <= n; i++){
+		w=a[i].s+a[i].t-1;
+        for(j = L; j >=w; j--)
+        {
+            f[j]=max(f[j-a[i].t]+1,f[j]);
+        }
+    }
+	for( i=0;i<=L;i++)ans=max(f[i],ans);
+	nemu_assert(ans==10);
+	return 0;
+}
+/*
+10
+16 10
+1 1
+1 1
+1 1
+1 1
+1 1
+1 1
+1 1
+1 1
+1 1
+1 1
+1 1
+1 1
+1 1
+1 1
+1 1
+1 1
+*/
