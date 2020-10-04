181e9cf042ccfa37cde165467bc0c2c565f24bcf
tracer-ics2016 <tracer@njuics.org>
2020-10-04 16:44:30 +0800

    >  compile NEMU 151220000 snow Linux snowNEMU 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:44:30 up  1:30,  1 user,  load average: 0.18, 0.16, 0.29 a86429d7aeff88427e1a00b72f31a318972e9e26

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -41,20 +41,34 @@ make_helper(rep) {
 }
 
 make_helper(repnz) {
+	int len;
 	int count = 0;
+	if(instr_fetch(eip + 1, 1) == 0xc3) {
+		/* repz ret */
+		exec(eip + 1);
+		len = 0;
+	}
+	else {
 	while(cpu.ecx) {
 		exec(eip + 1);
 		count ++;
 		cpu.ecx --;
 		assert(ops_decoded.opcode == 0xa6	// cmpsb
 				|| ops_decoded.opcode == 0xa7	// cmpsw
+				|| ops_decoded.opcode == 0xa4	// movsb
+				|| ops_decoded.opcode == 0xa5	// movsl
+				|| ops_decoded.opcode == 0xaa	// stos m8
+				|| ops_decoded.opcode == 0xab	// stos v
 				|| ops_decoded.opcode == 0xae	// scasb
 				|| ops_decoded.opcode == 0xaf	// scasw
 			  );
 
 		/* TODO: Jump out of the while loop if necessary. */
-
+		if((ops_decoded.opcode == 0xa6||ops_decoded.opcode==0xa7||ops_decoded.opcode==0xae||ops_decoded.opcode==0xaf)&&cpu.ZF==0)break;
 	}
+	len = 1;
+	}
+
 
 #ifdef DEBUG
 	char temp[80];
