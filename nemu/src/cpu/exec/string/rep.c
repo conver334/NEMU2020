fbb2f40efe480642c836ddb7098b82a614444760
tracer-ics2016 <tracer@njuics.org>
2020-10-04 16:45:58 +0800

    >  compile NEMU 151220000 snow Linux snowNEMU 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:45:58 up  1:32,  1 user,  load average: 0.16, 0.17, 0.28 c11bc1004f5d9b7949385e26ac144139c52b9548

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -66,9 +66,9 @@ make_helper(repnz) {
 		/* TODO: Jump out of the while loop if necessary. */
 		if((ops_decoded.opcode == 0xa6||ops_decoded.opcode==0xa7||ops_decoded.opcode==0xae||ops_decoded.opcode==0xaf)&&cpu.ZF==0)break;
 	}
-	len = 1;
+	
 	}
-
+	len = 1;
 
 #ifdef DEBUG
 	char temp[80];
