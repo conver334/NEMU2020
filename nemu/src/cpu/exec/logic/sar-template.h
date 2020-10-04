214035fc9f39422c4742026b8b90402eafbc76df
tracer-ics2016 <tracer@njuics.org>
2020-10-04 16:11:13 +0800

    >  compile NEMU 151220000 snow Linux snowNEMU 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:11:13 up 57 min,  1 user,  load average: 0.39, 0.52, 0.47 d9c368296843defdd04c2cdaef042118774fbfe6

--- nemu/src/cpu/exec/logic/sar-template.h
+++ nemu/src/cpu/exec/logic/sar-template.h
@@ -5,22 +5,35 @@
 static void do_execute () {
 	DATA_TYPE src = op_src->val;
 	DATA_TYPE_S dest = op_dest->val;
-	DATA_TYPE_S result = op_dest->val;
+
 	uint8_t count = src & 0x1f;
 	dest >>= count;
-	result = dest;
-	int len = (DATA_BYTE << 3) - 1;
-	cpu.CF=0;
-	cpu.OF=0;
-	cpu.SF=result >> len;
-    	cpu.ZF=!result;
-    	OPERAND_W(op_dest, dest);
-	result ^= result >>4;
-	result ^= result >>2;
-	result ^= result >>1;
-	cpu.PF=!(result & 1);
+	OPERAND_W(op_dest, dest);
 	
+	// concat(updateCPU_, SUFFIX) (dest);
+	/* There is no need to update EFLAGS, since no other instructions 
+	 * in PA will test the flags updated by this instruction.
+	 */
+
 	print_asm_template2();
+	// DATA_TYPE src = op_src->val;
+	// DATA_TYPE_S dest = op_dest->val;
+	// DATA_TYPE_S result = op_dest->val;
+	// uint8_t count = src & 0x1f;
+	// dest >>= count;
+	// result = dest;
+	// int len = (DATA_BYTE << 3) - 1;
+	// cpu.CF=0;
+	// cpu.OF=0;
+	// cpu.SF=result >> len;
+    // 	cpu.ZF=!result;
+    // 	OPERAND_W(op_dest, dest);
+	// result ^= result >>4;
+	// result ^= result >>2;
+	// result ^= result >>1;
+	// cpu.PF=!(result & 1);
+	
+	// print_asm_template2();
 }
 
 make_instr_helper(rm_1)
