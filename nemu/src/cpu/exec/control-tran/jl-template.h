106c7189a0782548fa3963aa4dbef3c75cba91cc
tracer-ics2016 <tracer@njuics.org>
2020-10-04 16:19:45 +0800

    >  compile NEMU 151220000 snow Linux snowNEMU 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:19:45 up  1:05,  1 user,  load average: 0.64, 0.58, 0.50 51cd5556fb64941e60cf878a3f42a7aac330bde

--- nemu/src/cpu/exec/control-tran/jl-template.h
+++ nemu/src/cpu/exec/control-tran/jl-template.h
@@ -1,14 +1,12 @@
-
 #include "cpu/exec/template-start.h"
 
 #define instr jl
 
-make_helper(concat(jl_i_, SUFFIX)) {
-	int len = concat(decode_i_, SUFFIX) (eip + 1);
-	print_asm("jl 0x%x", (DATA_TYPE_S)op_src->imm + cpu.eip + len + 1);
-	if(cpu.OF != cpu.SF) cpu.eip += (DATA_TYPE_S)op_src->imm;
-	return len + 1;
+static void do_execute() {
+	DATA_TYPE_S displacement = op_src->val;
+	print_asm("jl %x",cpu.eip + 1 + DATA_BYTE + displacement);
+	if (cpu.ZF == 0 && cpu.SF != cpu.OF)cpu.eip +=displacement;
 }
+make_instr_helper(i)
 
 #include "cpu/exec/template-end.h"
-
