6c3422d674b888b7d57e8bedcbca4ea5090f57be
3018216334-Zhang Simiao <1229797102@qq.com>
2020-10-21 22:01:11 +0800

    PA3->part1->define cache

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,10 +1,39 @@
 #include "common.h"
+/*
+cache block 存储空间的大小为 64B 
+cache 存储空间的大小为 64KB 
+8-way set associative 
+标志位只需要 valid bit 即可 
+替换算法采用随机方式 
+write through 
+not write allocate 
+
+8-way  set associative  每组有8行
+2^10 行 = 2^7组 * 8行/组
+*/
+#define BLOCK_SIZE 64
+#define CACHE_SIZE 64<<10
+#define ASSOCIATIVE_WAY 8
+
+struct Cache {
+	bool valid;
+	uint8_t data[BLOCK_SIZE];
+}cache[CACHE_SIZE/BLOCK_SIZE];
+
+void init_cache() {
+	int i, j, block_num=CACHE_SIZE/BLOCK_SIZE;
+	for(i = 0; i < block_num; i ++) {
+		cache[i].valid=0;
+		memset(cache[i].data,0,BLOCK_SIZE);
+	}
+}
+
 
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
 /* Memory accessing interfaces */
-
+//size_t size type 类型大小
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 }
