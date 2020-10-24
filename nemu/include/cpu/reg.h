adc750a36b1df1d06c04f37d6329de2f1c9a4bd2
Zhang Simiao <1229797102@qq.com>
2020-10-24 19:02:13 +0800

    PA3->part2->reg

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -2,11 +2,13 @@
 #define __REG_H__
 
 #include "common.h"
+#include "../../../lib-common/x86-inc/cpu.h" // import CR0 and CR3
 
 enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
 enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 
+enum { R_ES, R_CS, R_SS, R_DS, R_FS, R_GS};
 /* TODO: Re-organize the `CPU_state' structure to match the register
  * encoding scheme in i386 instruction format. For example, if we
  * access cpu.gpr[3]._16, we will get the `bx' register; if we access
@@ -14,24 +16,27 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
+typedef struct{
+	uint16_t selector;
+	uint16_t attribute;
+	uint32_t limit,base;
+}segment_reg;
+
 typedef struct {
-     union{
+    union{
         union {
-		uint32_t _32;
-		uint16_t _16;
-		uint8_t _8[2];
-	} gpr[8];
+			uint32_t _32;
+			uint16_t _16;
+			uint8_t _8[2];
+		} gpr[8];
 
 	/* Do NOT change the order of the GPRs' definitions. */
 
-	struct{
-            uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-        };
-     };
-
-     swaddr_t eip;
-
-     union {
+		struct{
+			uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		};
+	};
+	union {
 		struct {
 			uint32_t CF		:1;
 			uint32_t pad0	:1;
@@ -47,13 +52,70 @@ typedef struct {
 			uint32_t OF		:1;
 			uint32_t IOPL	:2;
 			uint32_t NT		:1;
-			uint32_t pad3	:1;
-			uint16_t pad4;
+			// uint32_t pad3	:1;
+			// uint16_t pad4;
+			uint32_t : 1;
+			uint32_t RF:1;
+			uint32_t VM:1;
+			uint32_t : 14;
 		};
-		uint32_t val;
-	} eflags;
+		uint32_t eflags;
+	};
+		 
+	
+	swaddr_t eip;
 
+	struct GDTR{
+		uint32_t base;
+		uint16_t limit;
+	}gdtr;
+
+	CR0 cr0;
+
+	union{
+		struct{
+			segment_reg sreg[6];
+		};
+		struct{
+			segment_reg es,cs,ss,ds,fs,gs;
+		};
+	};
+
+	CR3 cr3;
 } CPU_state;
+
+
+typedef struct{
+	union{
+		struct{
+			uint16_t limit1;
+			uint16_t base1;
+		};
+		uint32_t part1;
+	};
+	union{
+			struct{
+			uint32_t base2:		8;
+			uint32_t a:			1;
+			uint32_t type:		3;
+			uint32_t s:			1;
+			uint32_t dpl:		2;
+			uint32_t p:			1;
+			uint32_t limit2:	4;
+			uint32_t avl:		1;
+			uint32_t :			1;
+			uint32_t x:			1;
+			uint32_t g:			1;
+			uint32_t base3:		8;
+		};
+		uint32_t part2;
+	};
+}sreg_descriptor;
+sreg_descriptor *sreg_desc;
+
+uint8_t current_sreg;
+
+void sreg_load(uint8_t);
 
 extern CPU_state cpu;
 
