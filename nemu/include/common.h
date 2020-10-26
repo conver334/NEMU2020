82e3d6a56afc4183e8a2a760f931a98164ef9bfa
3018216334-Zhang Simiao <1229797102@qq.com>
2020-10-26 19:41:54 +0800

    m

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -25,7 +25,7 @@ typedef uint16_t ioaddr_t;
 
 #pragma pack (1)
 typedef union {
-	uint32_t _4;
+	uint32_t _4;	
 	uint32_t _3	: 24;
 	uint16_t _2;
 	uint8_t _1;
--- nemu/src/memory/dram.c
+++ nemu/src/memory/dram.c
@@ -1,7 +1,7 @@
 #include "common.h"
 #include "burst.h"
 #include "misc.h"
-
+#include "memory/cache.h"
 /* Simulate the (main) behavor of DRAM.
  * Although this will lower the performace of NEMU, it makes
  * you clear about how DRAM perform read/write operations.
