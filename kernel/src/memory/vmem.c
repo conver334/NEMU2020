a5fff8f1469ca1fe95aafd9af64403ab07469ff3
3018216334-Zhang Simiao <1229797102@qq.com>
2020-10-27 11:20:12 +0800

    PA3->part3->page addr

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -7,6 +7,7 @@
 
 /* Use the function to get the start address of user page directory. */
 PDE* get_updir();
+PTE vm_pt[NR_PTE] align_to_page;
 
 void create_video_mapping() {
 	/* TODO: create an identical mapping from virtual memory area 
