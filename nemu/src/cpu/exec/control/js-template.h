79a83aedb8e3755a03057752cb1c0a9395310b42
3018216334-Zhang Simiao <1229797102@qq.com>
2020-10-28 10:51:44 +0800

    PA3->part3->eflags debug

--- nemu/src/cpu/exec/control/js-template.h
+++ nemu/src/cpu/exec/control/js-template.h
@@ -5,7 +5,7 @@
 static void do_execute(){
     DATA_TYPE_S displacement = op_src -> val;
 	print_asm("js %x", cpu.eip + 1 + DATA_BYTE + displacement);
-	if(cpu.SF == 1) cpu.eip += displacement;
+	if(cpu.eflags.SF == 1) cpu.eip += displacement;
 }
 
 make_instr_helper(i)
