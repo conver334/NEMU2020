a7f7560fa5e965beb828daf1e0b8c7467fd3d6b2
tracer-ics2016 <tracer@njuics.org>
2020-10-28 17:02:20 +0800

    >  compile NEMU 10602121 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:02:20 up 48 min,  1 user,  load average: 1.43, 1.04, 0.95 89ddef13ebdb4c3d48439cd8bd4635e7e8809123

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -1,22 +1,56 @@
+// #include "cpu/exec/template-start.h"
+
+// #define instr scas
+
+// make_helper(concat(scas_, SUFFIX)) {
+// 	current_sreg = R_ES;
+
+// 	DATA_TYPE dest = REG(R_EAX);
+// 	DATA_TYPE src = MEM_R(cpu.edi);;
+// 	DATA_TYPE result = dest - src;
+
+// 	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+// 	cpu.eflags.CF = result > dest;
+// 	cpu.eflags.OF = MSB((dest ^ src) & (dest ^ result));
+
+// 	cpu.edi += (cpu.eflags.DF ? -DATA_BYTE : DATA_BYTE);
+
+// 	print_asm("scas" str(SUFFIX) " %%es:(%%edi),%%%s", REG_NAME(R_EAX));
+// 	return 1;
+// }
+
+// #include "cpu/exec/template-end.h"
+//--my pa2
 #include "cpu/exec/template-start.h"
 
 #define instr scas
 
-make_helper(concat(scas_, SUFFIX)) {
-	current_sreg = R_ES;
+make_helper(concat(scas_m_, SUFFIX)){
+    current_sreg = R_ES;
 
-	DATA_TYPE dest = REG(R_EAX);
-	DATA_TYPE src = MEM_R(cpu.edi);;
-	DATA_TYPE result = dest - src;
+    uint32_t num1 = REG(R_EAX);
+    uint32_t num2 = MEM_R(reg_l(R_EDI));
 
-	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
-	cpu.eflags.CF = result > dest;
-	cpu.eflags.OF = MSB((dest ^ src) & (dest ^ result));
+    uint32_t result = num1 - num2;
 
-	cpu.edi += (cpu.eflags.DF ? -DATA_BYTE : DATA_BYTE);
+    if(cpu.eflags.DF == 0) reg_l(R_EDI) += DATA_BYTE;
+    else reg_l(R_EDI) -= DATA_BYTE;
 
-	print_asm("scas" str(SUFFIX) " %%es:(%%edi),%%%s", REG_NAME(R_EAX));
-	return 1;
+    int len = DATA_BYTE * 8;
+    cpu.eflags.ZF = !result;
+    cpu.eflags.SF = result >> (len - 1);
+    cpu.eflags.CF = (num1 < num2);
+    int sign1 = (num1 >> (len - 1)), sign2 = (num2 >> (len - 1));
+    cpu.eflags.OF = (sign1 != sign2 && sign2 == cpu.eflags.SF);
+    result ^= result >> 4;
+    result ^= result >> 2;
+    result ^= result >> 1;
+    result &= 1;
+    cpu.eflags.PF = !result;
+
+    print_asm("scas");
+    return 1;
+
 }
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/string/scas.h
+++ nemu/src/cpu/exec/string/scas.h
@@ -1,8 +1,17 @@
+// #ifndef __SCAS_H__
+// #define __SCAS_H__
+
+// make_helper(scas_b);
+
+// make_helper(scas_v);
+
+// #endif
+//my pa2
 #ifndef __SCAS_H__
 #define __SCAS_H__
 
-make_helper(scas_b);
-
-make_helper(scas_v);
+make_helper(scas_m_b);
+make_helper(scas_m_v);
 
 #endif
+
