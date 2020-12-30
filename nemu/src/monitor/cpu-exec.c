245444bb7942bb8fbb59250c07be570a962f4399
3018216334-Zhang Simiao <1229797102@qq.com>
2020-12-30 11:15:12 +0800

    pa4-task2 hasdevice debug

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -105,8 +105,17 @@ void cpu_exec(volatile uint32_t n) {
 #endif
 
 		/* TODO: check watchpoints here. */
-		if(check_watchpoints()) nemu_state = STOP;
+		/* TODO: check watchpoints here. */
+        WP *wp = scan_watchpoint();
+		if(wp != NULL) {
+			puts(asm_buf);
+			printf("\n\nHint watchpoint %d at address 0x%08x, expr = %s\n", wp->NO, cpu.eip - instr_len, wp->expr);
+			printf("old value = %#08x\nnew value = %#08x\n", wp->old_val, wp->new_val);
+			wp->old_val = wp->new_val;
+			return;
+		}
 
+		if(nemu_state != RUNNING) { return; }
 #ifdef HAS_DEVICE
 		extern void device_update();
 		device_update();
